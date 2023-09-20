0c9124abf5f7c5e5d0900ada0b80155d1497792d
Abisual <host@a-utkin.ru>
2023-09-20 12:42:28 +0500

    problem with duplicates has been solved

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from lxml import html
 import time
 import random
 import sqlite3
+from datetime import datetime
 from urllib.parse import urljoin
 from config import query, container_xpath, title_xpath, price_xpath, link_xpath, \
     description_xpath, description_parts_xpath, user_agents
@@ -18,18 +19,73 @@ def create_table(conn):
             link TEXT,
             description TEXT,
             query TEXT,
-            unique_id TEXT
+            unique_id TEXT,
+            timestamp DATETIME DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'LOCALTIME')),
+            archived BOOLEAN DEFAULT 0
         )
     ''')
     conn.commit()
 
-def insert_into_db(conn, data, unique_id):
+
+
+def insert_into_db(conn, data, unique_id, timestamp):
     cursor = conn.cursor()
+
+    # Помечаем все объявления с таким же query как архивированные
     cursor.execute('''
-        INSERT INTO ads (title, price, link, description, query, unique_id) 
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', (data['title'], data['price'], data['link'], data['description'], query, unique_id))
+        UPDATE ads
+        SET archived=1
+        WHERE unique_id NOT IN (
+            SELECT unique_id FROM ads WHERE query=?
+        )
+    ''', (query,))
     conn.commit()
+
+    # Далее проверяем, есть ли уже в базе данных объявление с таким же unique_id
+    cursor.execute("SELECT * FROM ads WHERE unique_id=?", (unique_id,))
+    existing_data = cursor.fetchone()
+
+    if existing_data:
+        # Если объявление с таким unique_id уже есть в базе, сравниваем данные
+        if (
+                existing_data[1] != data['title'] or
+                existing_data[2] != data['price'] or
+                existing_data[3] != data['link'] or
+                existing_data[4] != data['description']
+        ):
+            # Собираем информацию о том, что изменилось
+            changes = []
+            if existing_data[1] != data['title']:
+                changes.append(f"Название: '{existing_data[1]}' -> '{data['title']}'")
+            if existing_data[2] != data['price']:
+                changes.append(f"Цена: '{existing_data[2]}' -> '{data['price']}'")
+            if existing_data[3] != data['link']:
+                changes.append(f"Ссылка: '{existing_data[3]}' -> '{data['link']}'")
+            if existing_data[4] != data['description']:
+                changes.append(f"Описание: '{existing_data[4]}' -> '{data['description']}'")
+
+            change_info = "\n  ".join(changes)
+
+            # Если хотя бы одно поле изменилось, обновляем данные
+            cursor.execute('''
+                UPDATE ads
+                SET title=?, price=?, link=?, description=?, query=?, unique_id=?, timestamp=?, archived=0
+                WHERE unique_id=?
+            ''', (
+            data['title'], data['price'], data['link'], data['description'], query, unique_id, timestamp, unique_id))
+            conn.commit()
+            print(f"Объявление с unique_id {unique_id} обновлено в базе данных. Изменения:\n  {change_info}")
+        else:
+            print(f"Объявление с unique_id {unique_id} не изменилось.")
+    else:
+        # Если объявление с таким unique_id не существует, вставляем его
+        cursor.execute('''
+            INSERT INTO ads (title, price, link, description, query, unique_id, timestamp) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''', (data['title'], data['price'], data['link'], data['description'], query, unique_id, timestamp))
+        conn.commit()
+        print(f"Добавлено новое объявление с unique_id {unique_id}.")
+
 
 def parse_drom(query):
     base_url = f"https://auto.drom.ru/search{query}"
@@ -38,6 +94,10 @@ def parse_drom(query):
     headers = {
         "User-Agent": random.choice(user_agents)
     }
+
+    # Создаем одно подключение к базе данных перед началом цикла
+    conn = sqlite3.connect("ads.db")
+    create_table(conn)
 
     while True:
         page_url = urljoin(base_url, f"page{current_page}/")
@@ -82,10 +142,11 @@ def parse_drom(query):
                     data = {"title": title, "price": price, "link": link, "description": description}
                     results.append(data)
 
-                    conn = sqlite3.connect("ads.db")
-                    create_table(conn)
-                    insert_into_db(conn, data, unique_id)
-                    conn.close()
+                    # Получаем текущую временную метку
+                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                    # Используем созданное подключение для вставки данных в базу
+                    insert_into_db(conn, data, unique_id, timestamp)
 
                 if results:
                     print(f"Результаты парсинга со страницы {current_page}:")
@@ -110,5 +171,9 @@ def parse_drom(query):
         else:
             print("Ошибка при выполнении запроса. Код ответа:", response.status_code)
             break
+
+    # Закрываем подключение к базе данных после завершения цикла
+    conn.close()
+
 print("Начинаем парсинг сайта Drom.ru")
 parse_drom(query)
